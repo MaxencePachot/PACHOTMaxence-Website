ff561c6a00ba0b49c2bd296dd4bb3ea1a6f65432
MaxencePachot <m.pachot@net.estia.fr>
2024-07-11 08:31:13 +0200

    correct the burger. It now closes when the page is changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles.css';
 
 const Navbar = () => {
+  const location = useLocation();
+  const navbarCollapseRef = useRef(null);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      const navDropdown = navbarCollapseRef.current;
+      if (navDropdown.classList.contains('show')) {
+        navDropdown.classList.remove('show');
+      }
+    };
+
+    handleRouteChange();
+
+    return () => {
+    };
+  }, [location]);
+
+  const handleLinkClick = () => {
+    const navDropdown = navbarCollapseRef.current;
+    if (navDropdown.classList.contains('show')) {
+      navDropdown.classList.remove('show');
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
       <div className="container">
@@ -10,16 +34,16 @@ const Navbar = () => {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown" ref={navbarCollapseRef}>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/" data-toggle="collapse" data-target="#navbarNavDropdown">Home</Link>
+              <Link className="nav-link" to="/" onClick={handleLinkClick}>Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/maxence_pachot_cv.pdf" target="_blank" rel="noopener noreferrer">CV</a>
+              <a className="nav-link" href="/maxence_pachot_cv.pdf" target="_blank" rel="noopener noreferrer" onClick={handleLinkClick}>CV</a>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/projects" data-toggle="collapse" data-target="#navbarNavDropdown">Projects</Link>
+              <Link className="nav-link" to="/projects" onClick={handleLinkClick}>Projects</Link>
             </li>
           </ul>
         </div>
