8c2a4b4cf9868ad3964e9bf826fbff43ec6a8f7b
MaxencePachot <m.pachot@net.estia.fr>
2025-07-08 16:35:52 +0200

    change yo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                   <h4 className="card-title">Maxence PACHOT</h4>
                 </center>
                 <p className="card-description">
-                  I'm a 23-year-old data engineer with a passion for data. Specialising in the design and optimisation of data pipelines, I work with large volumes of data and the integration of various sources.
+                  I'm a 25-year-old data engineer with a passion for data. Specialising in the design and optimisation of data pipelines, I work with large volumes of data and the integration of various sources.
                   Outside of work, I'm a keen football fan, enjoying both the game and analysing team performance data.
                 </p>
                 <center><a href="/maxence_pachot_cv.pdf" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-round">My CV</a>
